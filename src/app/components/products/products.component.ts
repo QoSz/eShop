e4c854ea735fb00ac979544c29e2578449a19fd2
Yash Shah <yashshah1282@gmail.com>
2023-07-30 00:14:37 +0300

    Trying to get the damn products page to work

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent {
 
+  shredders: {name: string; img: string;} [] = [
+    { name: 'Shreader 1', img: 'assets/Images/Shredders/shredder1.jpeg' },
+    { name: 'Shreader 2', img: 'assets/Images/Shredders/shredder2.jpeg' },
+  ];
+
+  files: {name: string; img: string;} [] = [
+    { name: 'File 1', img: 'assets/Images/Files/file1.avif' },
+    { name: 'File 2', img: 'assets/Images/Files/file2.avif' },
+    { name: 'File 3', img: 'assets/Images/Files/file3.avif' },
+    { name: 'File 4', img: 'assets/Images/Files/file4.avif' },
+    { name: 'File 5', img: 'assets/Images/Files/file5.avif' },
+    { name: 'File 6', img: 'assets/Images/Files/file1.avif' }
+  ];
 }
