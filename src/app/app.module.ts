51a589dee1a55af731c39891a1e4c5d7f1ee6eb9
Yash Shah <yashshah1282@gmail.com>
2023-08-17 22:55:04 +0300

    Completed Message us form. Working with EmailJs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +29,8 @@ import { ProductDetailsComponent } from './components/product-details/product-de
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent {
+  form: FormGroup = this.fb.group({
+    from_name: '',
+    to_name: 'Admin',
+    from_email: '',
+    subject: '',
+    message: '',
+  })
 
+  constructor(private fb: FormBuilder) {}
+
+  async send() {
+    emailjs.init("wXYIAyyF0geFeOR7N");
+    let response = await emailjs.send("service_j3acv8l", "template_7jmttni", {
+      from_name: this.form.value.from_name,
+      to_name: this.form.value.to_name,
+      from_email: this.form.value.from_email,
+      subject: this.form.value.subject,
+      message: this.form.value.message
+    });
+
+    alert("Message sent successfully!");
+    this.form.reset();
+  }
 }
